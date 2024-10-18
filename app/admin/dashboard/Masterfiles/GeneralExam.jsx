1ba3112c088b1bfb96411acbc0cb9317715b6439
Mel Macario <xmelmacario@gmail.com>
2024-10-18 10:31:05 +0800

    added card for general exam

--- app/admin/dashboard/Masterfiles/GeneralExam.jsx
+++ app/admin/dashboard/Masterfiles/GeneralExam.jsx
@@ -48,72 +48,78 @@ function GeneralExam() {
   return (
     <div className='rounded-md p-4'>
       <p className="text-2xl font-bold mb-4">General Exam</p>
-      {isLoading ? <Spinner /> :
-        <>
-          {!examMaster ? (
-            <div className='flex flex-col justify-center items-center gap-4'>
-              <p>No exam created yet</p>
-              <CreateExamMaster getSelectedJob={getGeneralExamDetails} type={1} />
-            </div>
-          ) : (
-            <>
-              <AddExamQuestion examId={examMaster.exam_id} getSelectedJob={getGeneralExamDetails} />
-              <div className='flex flex-col'>
-                <div className='flex md:justify-center'>
-                  <Card className='mb-3 flex flex-col gap-1 bg-background w-full md:w-1/2'>
-                    <CardContent className='p-3'>
-                      <div className='flex flex-row justify-between'>
-                        <h1 className='text-2xl font-boldtext-start'>
-                          {examMaster.exam_name}
-                        </h1>
-                        <UpdateExamMaster examMasterData={examMaster} getSelectedJob={getGeneralExamDetails} />
-                      </div>
-                      <p>Exam duration: {examMaster.exam_duration} minutes</p>
-                      <p className='text-sm'>Date created: {examMaster.exam_createdAt ? formatDate(examMaster.exam_createdAt) : 'N/A'}</p>
-                      <p className='text-sm'>Date updated: {examMaster.exam_updatedAt ? formatDate(examMaster.exam_updatedAt) : 'N/A'}</p>
-                    </CardContent>
-                  </Card>
-                </div>
-                {questionMaster.length === 0 ? (
-                  <div className='flex justify-center items-center mt-4'>
-                    <p>No questions have been added to this exam yet.</p>
+      <Card className="bg-[#0e5a35] dark:bg-[#1c1917]">
+        <CardContent>
+          <div className="p-3">
+            {isLoading ? <Spinner /> :
+              <>
+                {!examMaster ? (
+                  <div className='flex flex-col justify-center items-center gap-4'>
+                    <p>No exam created yet</p>
+                    <CreateExamMaster getSelectedJob={getGeneralExamDetails} type={1} />
                   </div>
                 ) : (
-                  <div className={`grid grid-cols-1 ${questionMaster.length === 1 ? 'md:grid-cols-1' : 'md:grid-cols-2'} gap-4`}>
-                    {questionMaster.map((question, index) => (
-                      <Card key={index} className='mb-3 bg-background w-full flex flex-col'>
-                        <CardContent className="p-3 flex-grow">
-                          <div className='grid grid-cols-3 gap-2'>
-                            <p className='mb-4 text-lg font-bold col-span-2'>{index + 1}. {question.examQ_text}</p>
-                            <div className='flex flex-row gap-4 justify-end md:mr-2'>
-                              <UpdateExamQuestion examQuestionData={question} getSelectedJob={getGeneralExamDetails} />
+                  <>
+                    <AddExamQuestion examId={examMaster.exam_id} getSelectedJob={getGeneralExamDetails} />
+                    <div className='flex flex-col'>
+                      <div className='flex md:justify-center'>
+                        <Card className='mb-3 flex flex-col gap-1 bg-background w-full md:w-1/2'>
+                          <CardContent className='p-3'>
+                            <div className='flex flex-row justify-between'>
+                              <h1 className='text-2xl font-boldtext-start'>
+                                {examMaster.exam_name}
+                              </h1>
+                              <UpdateExamMaster examMasterData={examMaster} getSelectedJob={getGeneralExamDetails} />
                             </div>
-                          </div>
-                          <RadioGroup defaultValue={question.options.find(opt => opt.examC_isCorrect === 1)?.examC_id.toString()}>
-                            {question.options.map((option, optionIndex) => (
-                              <div key={optionIndex} className="flex items-center space-x-2 mb-2">
-                                <RadioGroupItem
-                                  value={option.examC_id.toString()}
-                                  id={`${option.examC_id}`}
-                                  disabled
-                                  checked={option.examC_isCorrect === 1}
-                                />
-                                <Label htmlFor={`${option.examC_id}`}>{option.examC_text}</Label>
-                              </div>
-                            ))}
-                          </RadioGroup>
-                        </CardContent>
-                        <CardFooter className="mt-auto">
-                          <Badge className="ml-auto">Points {question.examQ_points}</Badge>
-                        </CardFooter>
-                      </Card>
-                    ))}
-                  </div>
+                            <p>Exam duration: {examMaster.exam_duration} minutes</p>
+                            <p className='text-sm'>Date created: {examMaster.exam_createdAt ? formatDate(examMaster.exam_createdAt) : 'N/A'}</p>
+                            <p className='text-sm'>Date updated: {examMaster.exam_updatedAt ? formatDate(examMaster.exam_updatedAt) : 'N/A'}</p>
+                          </CardContent>
+                        </Card>
+                      </div>
+                      {questionMaster.length === 0 ? (
+                        <div className='flex justify-center items-center mt-4'>
+                          <p>No questions have been added to this exam yet.</p>
+                        </div>
+                      ) : (
+                        <div className={`grid grid-cols-1 ${questionMaster.length === 1 ? 'md:grid-cols-1' : 'md:grid-cols-2'} gap-4`}>
+                          {questionMaster.map((question, index) => (
+                            <Card key={index} className='mb-3 bg-background w-full flex flex-col'>
+                              <CardContent className="p-3 flex-grow">
+                                <div className='grid grid-cols-3 gap-2'>
+                                  <p className='mb-4 text-lg font-bold col-span-2'>{index + 1}. {question.examQ_text}</p>
+                                  <div className='flex flex-row gap-4 justify-end md:mr-2'>
+                                    <UpdateExamQuestion examQuestionData={question} getSelectedJob={getGeneralExamDetails} />
+                                  </div>
+                                </div>
+                                <RadioGroup defaultValue={question.options.find(opt => opt.examC_isCorrect === 1)?.examC_id.toString()}>
+                                  {question.options.map((option, optionIndex) => (
+                                    <div key={optionIndex} className="flex items-center space-x-2 mb-2">
+                                      <RadioGroupItem
+                                        value={option.examC_id.toString()}
+                                        id={`${option.examC_id}`}
+                                        disabled
+                                        checked={option.examC_isCorrect === 1}
+                                      />
+                                      <Label htmlFor={`${option.examC_id}`}>{option.examC_text}</Label>
+                                    </div>
+                                  ))}
+                                </RadioGroup>
+                              </CardContent>
+                              <CardFooter className="mt-auto">
+                                <Badge className="ml-auto">Points {question.examQ_points}</Badge>
+                              </CardFooter>
+                            </Card>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  </>
                 )}
-              </div>
-            </>
-          )}
-        </>}
+              </>}
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
