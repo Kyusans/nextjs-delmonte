a78ea425976e3f2954f5eaca9c925c592e8affe9
Mel Macario <xmelmacario@gmail.com>
2024-10-18 12:21:31 +0800

    added delete training

--- app/admin/dashboard/Masterfiles/TrainingMaster.jsx
+++ app/admin/dashboard/Masterfiles/TrainingMaster.jsx
@@ -7,19 +7,67 @@ import AddTraining from './modal/AddMasterfileForms/AddTraining';
 import UpdateMasterfile from './modal/UpdateMasterfile';
 import { Trash2 } from 'lucide-react';
 import UpdateTraining from './modal/UpdateMasterfileForms/UpdateTraining';
+import ShowAlert from '@/components/ui/show-alert';
+
 const TrainingMaster = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
 
   const addColumn = (values, id) => {
     console.log("values", values);
     setData([...data, { ...values, perT_id: id }]);
   }
 
+  const handleShowAlert = (message) => {
+    setAlertMessage(message);
+    setShowAlert(true);
+  };
+
+  const handleCloseAlert = (status) => {
+    if (status === 1) {
+      handleDelete(selectedId);
+    }
+    setShowAlert(false);
+  };
+
+  const handleRemoveList = (trainingId) => {
+    setSelectedId(trainingId);
+    handleShowAlert("This action cannot be undone. It will permanently delete the item and remove it from your list");
+  };
+
+  const handleDelete = async (id) => {
+    setIsLoading(true);
+    try {
+      const url = process.env.NEXT_PUBLIC_API_URL + 'admin.php';
+      const formData = new FormData();
+      const jsonData = { trainingId: id }
+      formData.append("operation", "deleteTraining");
+      formData.append("json", JSON.stringify(jsonData));
+      const res = await axios.post(url, formData);
+      console.log("res.data ni handleDelete: ", res.data);
+      if (res.data === -1) {
+        toast.error("Failed to delete, there's a transaction using this training");
+      } else if (res.data === 1) {
+        toast.success("Training deleted successfully");
+        getData();
+      } else {
+        toast.error("Failed to delete training");
+      }
+    } catch (error) {
+      toast.error("Network error");
+      console.log("TrainingMaster.jsx ~ handleDelete ~ error:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   const columns = [
     { header: "Training", accessor: "perT_name" },
     {
-      header: "",
+      header: "Actions",
       cell: (row) => (
         <div className="flex gap-4">
           <UpdateTraining
@@ -28,7 +76,7 @@ const TrainingMaster = () => {
             currentName={row.perT_name}
             getData={getData}
           />
-          <Trash2 className="h-5 w-5 cursor-pointer" />
+          <Trash2 className="h-5 w-5 cursor-pointer" onClick={() => handleRemoveList(row.perT_id)} />
         </div>
       )
     }
@@ -49,7 +97,7 @@ const TrainingMaster = () => {
       }
     } catch (error) {
       toast.error("Network error");
-      console.log("SkillsMaster.jsx ~ getData ~ error:", error);
+      console.log("TrainingMaster.jsx ~ getData ~ error:", error);
     } finally {
       setIsLoading(false);
     }
@@ -80,7 +128,7 @@ const TrainingMaster = () => {
           />
         </>
       )}
-
+      <ShowAlert open={showAlert} onHide={handleCloseAlert} message={alertMessage} />
     </div>
   )
 }
