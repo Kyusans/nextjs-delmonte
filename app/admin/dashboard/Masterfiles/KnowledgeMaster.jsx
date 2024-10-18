e37001ce5cd3e1f1ba01ddc92bfe7b4d989408d0
Mel Macario <xmelmacario@gmail.com>
2024-10-18 09:14:08 +0800

    added handle delete for knowledge

--- app/admin/dashboard/Masterfiles/KnowledgeMaster.jsx
+++ app/admin/dashboard/Masterfiles/KnowledgeMaster.jsx
@@ -6,6 +6,7 @@ import { toast } from 'sonner';
 import AddKnowledge from './modal/AddMasterfileForms/AddKnowledge';
 import UpdateKnowledge from './modal/UpdateMasterfileForms/UpdateKnowledge';
 import { Trash2 } from 'lucide-react';
+import ShowAlert from '@/components/ui/show-alert';
 
 const KnowledgeMaster = () => {
   const [data, setData] = useState([]);
@@ -16,6 +17,51 @@ const KnowledgeMaster = () => {
       knowledge_id: newId,
       knowledge_name: values.knowledgeName
     }]);
+  }
+
+  // delete masterfile
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
+  const handleShowAlert = (message) => {
+    setAlertMessage(message);
+    setShowAlert(true);
+  };
+  const handleCloseAlert = (status) => {
+    if (status === 1) {
+      handleDelete(selectedId);
+    }
+    setShowAlert(false);
+  };
+  const handleRemoveList = (knowledgeId) => {
+    setSelectedId(knowledgeId);
+    handleShowAlert("This action cannot be undone. It will permanently delete the item and remove it from your list");
+  };
+
+  const handleDelete = async (id) => {
+    setIsLoading(true);
+    try {
+      const url = process.env.NEXT_PUBLIC_API_URL + 'admin.php';
+      const formData = new FormData();
+      const jsonData = { knowledgeId: selectedId }
+      formData.append("operation", "deleteKnowledge");
+      formData.append("json", JSON.stringify(jsonData));
+      const res = await axios.post(url, formData);
+      console.log("res.data ni handleDelete: ", res.data);
+      if (res.data === -1) {
+        toast.error("Failed to delete, there's a transaction using this knowledge");
+      } else if (res.data === 1) {
+        toast.success("Knowledge deleted successfully");
+        getData();
+      } else {
+        toast.error("Failed to delete knowledge");
+      }
+    } catch (error) {
+      toast.error("Network error");
+      console.log("KnowledgeMaster.jsx ~ handleDelete ~ error:", error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   const columns = [
@@ -30,7 +76,7 @@ const KnowledgeMaster = () => {
             currentName={row.knowledge_name}
             getData={getData}
           />
-          <Trash2 className="h-5 w-5 cursor-pointer" />
+          <Trash2 className="h-5 w-5 cursor-pointer" onClick={() => handleRemoveList(row.knowledge_id)} />
         </div>
       )
     }
@@ -82,6 +128,7 @@ const KnowledgeMaster = () => {
           />
         </>
       )}
+      <ShowAlert open={showAlert} onHide={handleCloseAlert} message={alertMessage} />
     </div>
   )
 }
