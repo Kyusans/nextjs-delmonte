e0e8cc212bef0aee4d1402c2c0146d5972d6ef1b
Mel Macario <xmelmacario@gmail.com>
2024-11-25 08:09:20 +0800

    added Background check page

---
+++ app/admin/dashboard/Job/BackgroundCheck/BackgroundCheckPage.jsx
@@ -0,0 +1,84 @@
+import DataTable from '@/app/my_components/DataTable'
+import { retrieveData } from '@/app/utils/storageUtils'
+import Spinner from '@/components/ui/spinner'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import SelectedApplicant from '../modal/SelectedApplicant'
+
+const BackgroundCheckPage = ({ handleChangeStatus }) => {
+  const [candidates, setCandidates] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCandId, setSelectedCandId] = useState(null);
+
+
+  const getBackgroundCheckCandidates = async () => {
+    setIsLoading(true)
+    try {
+      const url = process.env.NEXT_PUBLIC_API_URL + 'admin.php';
+      const jsonData = { jobId: retrieveData('jobId') };
+      const formData = new FormData();
+      formData.append("operation", "getBackgroundCheckCandidates");
+      formData.append("json", JSON.stringify(jsonData));
+      const res = await axios.post(url, formData);
+      console.log("res.data ni getBackgroundCheckCandidates: ", res);
+      setCandidates(res.data !== 0 ? res.data : []);
+    } catch (error) {
+      toast.error("Network error");
+      console.log("BackgroundCheckPage.jsx ~ getBackgroundCheckCandidates(): " + error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    getBackgroundCheckCandidates();
+  }
+
+  const handleOnClickRow = (id) => {
+    setSelectedCandId(id);
+    handleOpenModal();
+  };
+
+  useEffect(() => {
+    getBackgroundCheckCandidates();
+  }, []);
+
+  const columns = [
+    { header: "Full Name", accessor: "fullName" },
+    { header: "Status", accessor: "status_name" },
+  ];
+
+  return (
+    <div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <DataTable
+          columns={columns}
+          data={candidates}
+          autoIndex={true}
+          onRowClick={handleOnClickRow}
+          idAccessor="cand_id"
+        />
+
+      )}
+      {isModalOpen &&
+        <SelectedApplicant
+          open={isModalOpen}
+          onHide={handleCloseModal}
+          statusName="Background Check"
+          candId={selectedCandId}
+          handleChangeStatus={handleChangeStatus}
+        />
+      }
+    </div>
+  )
+}
+
+export default BackgroundCheckPage
